2f1a05f53b956ca99d67af4adc72d096fdc4e75b
Adwaid Krishna K <adwaidkrishna44@gmail.com>
2024-06-14 15:11:17 +0530

    commit

---
+++ Day 8.sql
@@ -0,0 +1,35 @@
+create database amazon;
+use amazon;
+create table products
+(
+	pid int(3) primary key,
+    pname varchar(50) not null,
+    price int(10) not null,
+    stock int(5),
+    location varchar(30) check(location in ('Mumbai','Delhi'))
+);
+create table customer
+(
+	cid int(3) primary key,
+    cname varchar(30) not null,
+    age int(3),
+    addr varchar(50)
+);
+create table orders
+(
+	oid int(3) primary key,
+    cid int(3),
+    pid int(3),
+    amt int(10) not null,
+    foreign key(cid) references customer(cid),
+    foreign key(pid) references products(pid)
+);
+create table payment
+(
+	pay_id int(3) primary key,
+    oid int(3),
+    amount int(10) not null,
+    mode varchar(30) check(mode in('upi','credit','debit')),
+    status varchar(30),
+    foreign key(oid) references orders(oid)
+);
